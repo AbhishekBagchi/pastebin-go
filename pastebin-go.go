0cf117848487a2db1a289aeadb6d8e275de5f6d5
Abhishek Bagchi <me@abhishekbagchi.co.uk>
2020-03-13 17:17:17 +0000

    Handle get and post in same handler

--- pastebin-go.go
+++ pastebin-go.go
@@ -14,24 +14,24 @@ import (
 var templates = template.Must(template.ParseGlob(filepath.Join(os.Getenv("TEMPLATE_DIR"), "*.html")))
 var hash = sha256.New()
 
-func pasteHandler(w http.ResponseWriter, r *http.Request) {
-    data := []byte(r.FormValue("contents"))
-    hash := hash.Sum(data)
-    filename := hex.EncodeToString(hash[0:8])
-
-    //FIXME First implimentation. Can and should be improved.
-    err := ioutil.WriteFile(filename, data, 0644)
-    if (err != nil) {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-}
-
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-    err := templates.ExecuteTemplate(w, "indexPage", nil)
-    if (err != nil) {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
+    if (r.Method == http.MethodGet) {
+        err := templates.ExecuteTemplate(w, "indexPage", nil)
+        if (err != nil) {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+    } else if (r.Method == http.MethodPost) {
+        data := []byte(r.FormValue("contents"))
+        hash := hash.Sum(data)
+        filename := hex.EncodeToString(hash[0:8])
+
+        //FIXME First implimentation. Can and should be improved.
+        err := ioutil.WriteFile(filename, data, 0644)
+        if (err != nil) {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
     }
 }
 
@@ -40,6 +40,5 @@ func main() {
     http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(os.Getenv("STATIC_DIR")))))
 
     http.HandleFunc("/", indexHandler)
-    http.HandleFunc("/paste", pasteHandler)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
