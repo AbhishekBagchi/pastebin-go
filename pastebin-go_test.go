e48be157a1345ed5a3fb1c87a336b790bbf8d849
abhishek <abhishekbagchi@ymail.com>
2020-07-25 20:13:40 +0100

    Fix test

--- pastebin-go_test.go
+++ pastebin-go_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestEncodeDecode(t *testing.T) {
 	data := []byte("Hello World")
-	data = encodeTime(data, 1*time.Minute)
+	_, data = encodeTime(data, 1*time.Minute)
 	canary := []byte{0xde, 0xad}
 	if data[0] != canary[0] || data[1] != canary[1] {
 		t.Error("Malformed data")
